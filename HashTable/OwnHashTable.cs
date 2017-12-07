a7207a2f51cf9f44f7a02ac8e5bdffc67e680e71
Yuliia <skylenko@gmail.com>
2017-12-07 16:24:44 +0200

    Created hashTable project

---
+++ HashTable/OwnHashTable.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace HashTable
+{
+    public class OwnHashTable : IHashTable
+    {
+        public int Size;
+
+        private class HashTableEntry
+        {
+            internal object Key;
+            internal object Value;
+        }
+
+        HashTableEntry[] _tableEntries = new HashTableEntry[100];
+
+        public bool Contains(object key)
+        {
+            int index = Index(key);
+
+            if (_tableEntries[index] == null)
+            {
+                return false;
+            }
+            return true;
+        }
+        
+        private static int IndexFor(int h, int length)
+        {
+            return h & (length - 1);
+        }
+
+
+        private int Index(object key)
+        {
+            int hashCode = key.GetHashCode();
+            int index = IndexFor(hashCode, _tableEntries.Length);
+            return index;
+        }
+
+        public void Add(object key, object value)
+        {
+            HashTableEntry hashTableEntry = new HashTableEntry();
+
+            int index = Index(key);
+
+            if (Contains(key))
+            {
+                throw new Exception("Key already exists in the hash table");
+            }
+
+            hashTableEntry.Key = key;
+            hashTableEntry.Value = value;
+
+            _tableEntries[index] = hashTableEntry;
+
+            Size++;
+        }
+        
+        public object this[object key]
+        {
+            get
+            {
+                if (!Contains(key))
+                {
+                    throw new Exception("Key does not exist in the hash table");
+                }
+                int index = Index(key);
+
+                return _tableEntries[index].Value;
+            }
+
+            set
+            {
+                int index = Index(key);
+
+                if (value == null)
+                {
+                    _tableEntries[index] = null;
+                }
+
+                else
+                {
+                    _tableEntries[index].Value = value;
+                }
+            }
+        }
+
+        public bool TryGet(object key, out object value)
+        {
+            int index = Index(key);
+            HashTableEntry res = _tableEntries[index];
+            value = res.Value;
+            return value != null;
+        }
+    }
+}
---
+++ Test/Tests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using NUnit.Framework;
+using HashTable;
+
+namespace Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        private OwnHashTable _hashTable;
+
+        [SetUp]
+        public void Init()
+        {
+            _hashTable = new OwnHashTable();
+        }
+
+        [Test]
+        public void ShouldContainElement()
+        {
+            _hashTable.Add("Test", "one");
+            Assert.True(_hashTable.Contains("Test"));
+        }
+
+        [Test]
+        public void ShouldNotContainKeyWhenEmpty()
+        {
+            Assert.False(_hashTable.Contains("qwe"));
+        }
+
+        [Test]
+        public void ShouldAddKeyValue()
+        {
+            _hashTable.Add("Something", 9);
+            Assert.AreEqual(_hashTable.Size, 1);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionAddMethod()
+        {
+            Exception exception = null;
+            _hashTable.Add("test", 8);
+
+            try
+            {
+                _hashTable.Add("test", 0);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            if (exception != null) Assert.AreEqual(exception.Message, "Key already exists in the hash table");
+        }
+
+        [Test]
+        public void ShouldGetValueByKey()
+        {
+            _hashTable.Add("Flower", "Red");
+            Assert.True(_hashTable["Flower"].Equals("Red"));
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenKeyDoesNotExist()
+        {
+            Exception exception = null;
+            _hashTable.Add(4.44, 'T');
+
+            try
+            {
+                var o = _hashTable["i"];
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            if (exception != null) Assert.AreEqual(exception.Message, "Key does not exist in the hash table");
+        }
+
+        [Test]
+        public void ShouldSetValueByKey()
+        {
+            _hashTable.Add("Mark", 98);
+            _hashTable["Mark"] = 89;
+            Assert.True(_hashTable["Mark"].Equals(89));
+        }
+
+        [Test]
+        public void ShouldDeletePairWhenTrySetNullValueByKey()
+        {
+            _hashTable.Add("Sun", 98);
+            _hashTable["Sun"] = null;
+            Exception exception = null;
+
+            try
+            {
+                var o = _hashTable["Sun"];
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            if (exception != null) Assert.AreEqual(exception.Message, "Key does not exist in the hash table");
+        }
+
+        [Test]
+        public void ShouldTryGetKeyValue()
+        {
+            _hashTable.Add(9, 8);
+            object o;
+            Assert.True(_hashTable.TryGet(9, out o));
+        }
+
+        [Test]
+        public void ShouldNotContainPairEmpty()
+        {
+            _hashTable.Add(9, null);
+            object o;
+            Assert.False(_hashTable.TryGet(9, out o));
+        }
+    }
+}
